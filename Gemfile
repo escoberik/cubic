169e9e92313aaea93d2499c5775d9c144c4a5bce
Erik Escobedo <erik@mojotech.com>
2011-12-30 03:19:54 -0600

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+gem 'sqlite3'
+
+gem 'haml-rails'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'turn', '0.8.2', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cubic::Application.config.secret_token = '4e7efebc53a812fcf4d5c98ad5bbe17b31d3e4d41f656237930be1378e3fbf28cfa62d1127d4fd504a9bd40a76152d5ff818b18cbb0911f4901244ccf5911ef9'
